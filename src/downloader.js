f89677966ee87480564293e0b18a1eac41a78adf
Bhanu Reddy <bhanur@jfrog.com>
2023-01-23 18:01:29 +0530

    Removed version from input

--- src/downloader.js
+++ src/downloader.js
@@ -8,45 +8,18 @@ async function download(targetFolder, cacheIntegration, cacheRepository) {
   if (!cacheIntegration || !cacheRepository) {
     tasks.warning("Cache configuration not set. Caching will be skipped.");
   }
-  // const pathToFile = await tasks.downloadFile(goUrl, targetFolder, cacheRepository, cacheIntegration);
-  // await extractPackage(pathToFile, targetFolder);
   await tasks.execute(url)
 }
 
 function computeDownloadUrl() {
   let nodeVersion = tasks.execute(`node --version`).then(r => r.stdOut)
+  tasks.info("received node version is "+ nodeVersion)
 
   let isNewNodeVersion = semver.gte(nodeVersion, '16.10.0')
   if (isNewNodeVersion) {
     return 'corepack enable'
   }
   return 'npm i -g corepack'
-}
-
-function getOsFamily() {
-  return tasks.getOperatingSystemFamily().toLowerCase();
-}
-
-function getArchitecture() {
-  const arch = tasks.getArchitecture();
-  if (arch === "x86_64") return "amd64";
-  if (arch === "ARM64") return "arm64";
-  throw new tasks.PipelinesTaskError("Architecture not supported");
-}
-
-function getPackageType(osFamily) {
-  if (osFamily === "windows") return "zip";
-  return "tar.gz";
-}
-
-async function extractPackage(pathToPackage, targetFolder) {
-  tasks.info("Extracting package content");
-  const osFamily = getOsFamily();
-  const packageType = getPackageType(osFamily);
-  if (packageType === "zip")
-    await tasks.unzip(pathToPackage, targetFolder);
-  else
-    await tasks.untar(pathToPackage, targetFolder);
 }
 
 module.exports = {
--- src/setup.js
+++ src/setup.js
@@ -5,54 +5,6 @@ const fs = require("fs");
 const os = require("os");
 const downloader = require("./downloader");
 
-let inputVersion;
-let inputCacheIntegration;
-let inputCacheRepository;
-
-function readAndValidateInput() {
-  inputVersion = tasks.getInput("version");
-  if (!inputVersion) throw "version input is required";
-
-  inputVersion = semver.valid(inputVersion);
-  if (!inputVersion) throw "version input must be semver compatible";
-
-  inputCacheIntegration = tasks.getInput("cacheIntegration");
-  inputCacheRepository = tasks.getInput("cacheRepository");
-}
-
-function findArtifactoryIntegration(inputCacheIntegration) {
-  if (inputCacheIntegration) {
-    const currentIntegration = tasks.getIntegration(inputCacheIntegration);
-    if (currentIntegration.masterName.toLowerCase() === "artifactory") {
-      return currentIntegration;
-    } else {
-      throw (
-        "Input cacheIntegration is not an Artifactory Integration. Type: " +
-        currentIntegration.masterName
-      );
-    }
-  } else {
-    tasks.info("Searching for Artifactory integration");
-    try {
-      const currentIntegration = tasks.findIntegrationByType("artifactory");
-      tasks.info(`Artifactory integration ${currentIntegration.name} founded!`);
-      return currentIntegration;
-    } catch (err) {
-      if (err instanceof tasks.IntegrationNotFound) {
-        return undefined;
-      } else {
-        throw err;
-      }
-    }
-  }
-}
-
-function createTargetFolder() {
-  const goFolder = path.join(tasks.getStepWorkspaceDir(), "go");
-  fs.mkdirSync(goFolder, { recursive: true });
-  return goFolder;
-}
-
 function run() {
   let downloadStr = downloader.download("", "", "").then(r => r.toString())
 }
