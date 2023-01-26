178d9de3f9cf2203e640b875ad9d6683571cc5cd
Bhanu Reddy <bhanur@jfrog.com>
2023-01-26 13:03:26 +0530

    removed which yarn command

--- src/info.js
+++ src/info.js
@@ -2,7 +2,7 @@ const tasks = require("jfrog-pipelines-tasks");
 
 function writeInfo() {
   tasks.execute('yarn --version').then(r => r.stdOut)
-  tasks.execute('which yarn').then(r => r.stdOut)
+  //tasks.execute('which yarn').then(r => r.stdOut)
 }
 
 module.exports = {
