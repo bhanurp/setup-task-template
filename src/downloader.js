b2dc185e82284de392bb80f191abc73426535012
Bhanu Reddy <bhanur@jfrog.com>
2023-01-28 10:34:07 +0530

    Revert "Removed async functions"
    This reverts commit 4c748edcc960c1dd4e33b02f69b178205e2e73c9.

--- src/downloader.js
+++ src/downloader.js
@@ -3,9 +3,9 @@ const semver = require("semver");
 const resolve = require("resolve");
 const {error} = require("jfrog-pipelines-tasks");
 
-function download(targetFolder, cacheIntegration, cacheRepository) {
+async function download(targetFolder, cacheIntegration, cacheRepository) {
 
-  const url = computeDownloadUrl().then((message) => {
+  const url = await computeDownloadUrl().then((message) => {
     return message
   }).catch((message) => {
     tasks.error("failed to resolve download url")
@@ -16,8 +16,8 @@ function download(targetFolder, cacheIntegration, cacheRepository) {
   }
 }
 
-function computeDownloadUrl() {
-  const {stdOut, stdErr} = tasks.execute("node --version")
+async function computeDownloadUrl() {
+  const {stdOut, stdErr} = await tasks.execute("node --version")
   if (stdErr) {
     tasks.error(stdErr)
     return Promise.reject('failed to fetch node version')
@@ -27,10 +27,10 @@ function computeDownloadUrl() {
   let isNewNodeVersion = semver.gte(stdOut.toString(), '16.10.0')
   tasks.info("node version:"+isNewNodeVersion)
   if (isNewNodeVersion) {
-    tasks.execute("corepack enable")
+    await tasks.execute("corepack enable")
     return Promise.resolve('success')
   }
-  tasks.execute("npm i -g corepack")
+  await tasks.execute("npm i -g corepack")
   return Promise.resolve('success')
 }
 
