517758a725b5e1d0482ec2bcc8edb96ae3b99213
Bhanu Reddy <bhanur@jfrog.com>
2023-01-28 11:39:53 +0530

    Converted stdout to string

--- src/info.js
+++ src/info.js
@@ -4,10 +4,10 @@ const {error} = require("jfrog-pipelines-tasks");
 async function writeInfo() {
   const{stdout, stderr} = await tasks.execute('yarn --version')
   if (stderr){
-    tasks.error(stderr)
+    tasks.error(stderr.toString())
     throw error("Failed to fetch yarn version.")
   }
-  tasks.info("Installed yarn version is:"+stdout)
+  tasks.info("Installed yarn version is:"+stdout.toString())
 }
 
 module.exports = {
