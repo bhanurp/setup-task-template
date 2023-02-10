99f6c1be8bbc468e999b133fbc4bedd591b5ebdd
Bhanu Reddy <bhanur@jfrog.com>
2023-02-10 14:03:32 +0530

    Updated with handling of standard buffers

--- src/downloader.js
+++ src/downloader.js
@@ -1,16 +1,7 @@
 const tasks = require("jfrog-pipelines-tasks");
-const semver = require("semver");
-const resolve = require("resolve");
-const {error} = require("jfrog-pipelines-tasks");
 
 async function download(targetFolder, cacheIntegration, cacheRepository) {
-
-  const url = await computeDownloadUrl().then((message) => {
-   tasks.info(message)
-  }).catch((message) => {
-    tasks.error("failed to resolve download url")
-    return error(message)
-  })
+  const url = await computeDownloadUrl()
   if (!cacheIntegration || !cacheRepository) {
     tasks.warning("Cache configuration not set. Caching will be skipped.");
   }
@@ -18,19 +9,28 @@ async function download(targetFolder, cacheIntegration, cacheRepository) {
 }
 
 async function computeDownloadUrl() {
-  const {stdOut, stdErr} = await tasks.execute("node --version")
-  if (stdErr) {
-    tasks.error(stdErr)
-    await Promise.reject('failed to fetch node version')
+  try {
+    const commandOutput = await tasks.execute("node --version")
+    handleCommandOutput(commandOutput)
+    tasks.info("Received node version is:" + stdOut.toString())
+    const installCommandOutput = await tasks.execute("npm install --global yarn")
+    handleCommandOutput(installCommandOutput)
+  } catch (e) {
+    throw new Error(e);
   }
-  tasks.info("Received node version is:" + stdOut.toString())
+}
 
-  let isNewNodeVersion = semver.gte(stdOut.toString(), '16.10.0')
-  tasks.info("node version:"+isNewNodeVersion)
-  if (isNewNodeVersion) {
-    await tasks.execute("npm install --global yarn")
+
+/**
+ * Handles command execution output stdOut and stdErr
+ * @param commandOutput
+ */
+function handleCommandOutput(commandOutput) {
+  if (commandOutput.stdErr){
+    tasks.error(commandOutput.stdErr)
+  } else {
+    tasks.info(commandOutput.stdOut);
   }
-  await tasks.execute("npm install --global yarn")
 }
 
 module.exports = {
