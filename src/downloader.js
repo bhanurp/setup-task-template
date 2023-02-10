9ccff3700008dd2e636cbcb3b76c549fe01301aa
Bhanu Reddy <bhanur@jfrog.com>
2023-02-10 11:40:14 +0530

    Updated yarn installation method

--- src/downloader.js
+++ src/downloader.js
@@ -28,9 +28,9 @@ async function computeDownloadUrl() {
   let isNewNodeVersion = semver.gte(stdOut.toString(), '16.10.0')
   tasks.info("node version:"+isNewNodeVersion)
   if (isNewNodeVersion) {
-    await tasks.execute("corepack enable")
+    await tasks.execute("npm install --global yarn")
   }
-  await tasks.execute("npm i -g corepack")
+  await tasks.execute("npm install --global yarn")
 }
 
 module.exports = {
